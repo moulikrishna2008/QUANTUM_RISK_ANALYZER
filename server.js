a523b238959b83c5f37ea5f08b4196789404c141
MOULI KRISHNA <penugondasrimouli@mictech.edu.in>
2026-07-26 22:46:16 +0530

    Update server.js

--- server.js
+++ server.js
@@ -12,10 +12,12 @@ app.use(express.json());
    FIREBASE SETUP
 ========================= */
 
-const serviceAccount = require("./serviceAccountKey.json");
-
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
+  credential: admin.credential.cert({
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+    privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+  }),
 });
 
 const db = admin.firestore();
@@ -28,7 +30,7 @@ const transporter = nodemailer.createTransport({
   port: 587,
   secure: false,
   auth: {
-   user: process.env.GMAIL_USER,
+    user: process.env.GMAIL_USER,
     pass: process.env.GMAIL_PASS,
   },
   tls: {
